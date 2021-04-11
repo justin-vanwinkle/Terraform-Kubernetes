ed08de56fac5f499362435ab389cc75a00a75cfe
Justin VanWinkle <vanwinkle.justin@gmail.com>
2021-04-11 15:03:25 -0400

    state

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-# terraform {
-#  backend "azurerm" {
-#    storage_account_name = "sttfstate1"
-#    container_name       = "tfstate"
-#    key                  = "aks.tfstate"
-#  }
-# }
+terraform {
+ backend "azurerm" {
+   storage_account_name = "sttfstate1"
+   container_name       = "tfstate"
+   key                  = "aks.tfstate"
+ }
+}
 
 provider "azurerm" {
   version = "~> 2.16.0"
