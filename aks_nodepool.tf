da76ca26505c77172a1b24675160167a052b89ba
Justin VanWinkle <vanwinkle.justin@gmail.com>
2021-04-11 22:44:16 -0400

    spot node size

--- aks_nodepool.tf
+++ aks_nodepool.tf
@@ -4,6 +4,6 @@ resource "azurerm_kubernetes_cluster_node_pool" "spot" {
   eviction_policy       = "Delete"
   spot_max_price        = 1
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS2_v2"
+  vm_size               = "Standard_D4DS_V4"
   node_count            = 1
 }
